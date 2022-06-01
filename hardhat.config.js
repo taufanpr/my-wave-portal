2a07580c59f1044b0420a34292f0d726bdb2fcd2
taufanpr <35000532+taufanpr@users.noreply.github.com>
2022-06-01 16:00:53 +0700

    q1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "YOUR_ALCHEMY_API_URL XXX",
-      accounts: ["YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY"]
+      // This value will be replaced on runtime
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
